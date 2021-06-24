7bc7057684c8ed318809e3626da2cde793a81a0f
rodrigo <rodrigoveloso1t@gmail.com>
2021-06-24 11:28:05 -0300

    1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+import matplotlib.pyplot as plt
 
 st.write("""# Hypothesis test""")
 
@@ -27,15 +28,153 @@ with st.echo():
 st.write(df.head(5))
 
 st.write("""## Normality Test""")
+st.write(""" Tests whether a data sample has a Gaussian distribution. The Shapiro-Wilk Test is set as default. One can also apply D’Agostino’s $K^2$ Test""")
+
+st.write("""### Default test example""")
+
 with st.echo():
     p = Preprocessing()
     df = p.clean_data(df)
     t = Tester()
-    results = t.normality_test(df['Age'], test='shapiro-wilk')
+    results = t.normality_test(df['Age'])
     sns_plot = sns.distplot(df['Age'])
     print(results)
     print("p-value = {}, significance = {}".format(results.p_value, results.significance))
 
 st.pyplot(sns_plot.figure)
+st.write(results)
+st.write("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+plt.clf()
+
+st.write("""### D’Agostino’s $K^2$  test example""")
+
+with st.echo():
+    p = Preprocessing()
+    df = p.clean_data(df)
+    t = Tester()
+    results = t.normality_test(df['Age'][:30], test = 'dagostinos')
+    sns_plot = sns.distplot(df['Age'][:30])
+    print(results)
+    print("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.pyplot(sns_plot.figure)
+st.write(results)
+st.write("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write("""## Dependency test between two categorical variables""")
+st.write(""" Tests whether two categorical variables are related or independent. Using the default configuration: If all the cells of contigency table have values above 5, Chi-Squared test is used, otherwise the normality condition is not satisfied for Chi-Squared and dimensions of contigency table are checked. If the contigency table is 2x2 than both Fisher-Exact test and Chi-Squared test are applied, else Chi-Squared test is applied even though the normality condition is not satisfied. One can also specifies what test should be applied using the test parameter.""")
+
+st.write("""### Normality condition satisfied for Chi-Squared example""")
+
+with st.echo():
+    p = Preprocessing()
+    df = p.clean_data(df)
+    t = Tester()
+    results = t.categorical_test(df['Survived'], df['Sex'])
+    print(results)
+    print("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write(results)
+st.write("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write("""### Normality condition not satisfied for Chi-Squared example""")
+
+with st.echo():
+    p = Preprocessing()
+    df = p.clean_data(df)
+    t = Tester()
+    results = t.categorical_test(df['Survived'][:20], df['Sex'][:20])
+    print(results)
+    print(results.chi2)
+    print("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write(results)
+st.write(results.chi2)
+st.write("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write("""### Normality condition not satisfied for Chi-Squared and contigency table not 2x2 example""")
+
+with st.echo():
+    p = Preprocessing()
+    df = p.clean_data(df)
+    t = Tester()
+    results = t.categorical_test(df['Survived'][:20], df['Pclass'][:20])
+    print(results)
+    print("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write(results)
+st.write("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write("""### Using just a specific test example (no checks are made)""")
+
+with st.echo():
+    p = Preprocessing()
+    df = p.clean_data(df)
+    t = Tester()
+    results = t.categorical_test(df['Survived'][:20], df['Pclass'][:20], test = 'chi2')
+    print(results)
+    print("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write(results)
+st.write("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write("""## Correlation test between two quantitative variables""")
+st.write(""" Tests whether two quantitative variables have a linear relationship. Using the default configuration: If both variables are binary, Pearson test is applied (Point-biserial). If variables are not binary and have a normal distribution, Pearson test is also applied, else Spearmen test is applied. One can also specifies what test should be applied using the test parameter.""")
+
+st.write("""### Binary variables example""")
+
+with st.echo():
+    p = Preprocessing()
+    df = p.clean_data(df)
+    df = p.categ_encoding(df)
+    t = Tester()
+    results = t.categorical_test(df['Survived'][:20], df['Sex_female'][:20])
+    print(results)
+    print("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write(results)
+st.write("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write("""### Normally distributed example""")
+
+with st.echo():
+    p = Preprocessing()
+    df = p.clean_data(df)
+    df['2xAge'] = df['Age']*2
+    t = Tester()
+    results = t.correlation_test(df['2xAge'][:20], df['Age'][:20])
+    print(results)
+    print("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write(results)
+st.write("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write("""### Not normally distributed example""")
+
+with st.echo():
+    p = Preprocessing()
+    df = p.clean_data(df)
+    df['2xAge'] = df['Age']*2
+    t = Tester()
+    results = t.correlation_test(df['2xAge'], df['Age'])
+    print(results)
+    print("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write(results)
+st.write("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
+st.write("""### Using just a specific test example (no checks are made)""")
+
+with st.echo():
+    p = Preprocessing()
+    df = p.clean_data(df)
+    t = Tester()
+    df['2xAge'] = df['Age']*2
+    t = Tester()
+    results = t.correlation_test(df['2xAge'], df['Age'], test = 'kendalltau')
+    print(results)
+    print("p-value = {}, significance = {}".format(results.p_value, results.significance))
+
 st.write(results)
 st.write("p-value = {}, significance = {}".format(results.p_value, results.significance))
--- ml/preprocessing/hypothesis_testing.py
+++ ml/preprocessing/hypothesis_testing.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 from scipy.stats import shapiro
 from scipy.stats import normaltest
+from scipy.stats import chi2_contingency
+from scipy.stats import fisher_exact
+from scipy.stats import pearsonr
+from scipy.stats import spearmanr
+from scipy.stats import kendalltau
 
 class TestResult:
 
@@ -53,7 +58,137 @@ class Tester:
         Tester
         """
         self.selectors = {'normality_test':{'shapiro-wilk':shapiro,
-                                                                 'dagostinos':normaltest}}
+                                                                 'dagostinos':normaltest},
+                                    'compare_2_categorical':{'chi2':chi2_contingency,
+                                                                                'fisher_exact':fisher_exact},
+                                    'correlation_test':{'pearson':pearsonr,
+                                                                   'spearman':spearmanr,
+                                                                   'kendalltau':kendalltau}}
+
+    def correlation_test(self, col1, col2, significance = 0.05, test = 'default', binary = ''):
+        """
+        Tests the null hypothesis that there is no correlation between quantitative samples (col2,col2)
+        
+    	Parameters
+    	----------            
+        col1 : array_like
+                  Array of sample data, must be quantitative data.
+        col1 : array_like
+                  Array of sample data, must be quantitative data.
+        significance : float
+                               level of significance (default = 0.05)
+        test : string
+                 correlation test to be applied
+        binary : string
+                      flag to identify if data is binary
+                    
+    	Returns
+    	-------
+        TestResult
+        """
+        report = ""
+        if test == 'default':
+            if binary == 'yes':
+                check = True
+            elif binary == 'no':
+                check = False
+            else:
+                check1 = self.check_binary(col1)
+                check2 = self.check_binary(col2)
+                check = check1 and check2
+
+            if check:
+                report += "Samples are binary, Pearson correlation is going to be applied (Point-biserial). "
+                return self.correlation(col1, col2, 'pearson', significance, report)
+            else:
+                check1 = self.normality_test(col1).result
+                check2 = self.normality_test(col2).result
+                check = check1 and check2
+                if check:
+                    report += "Samples have normal distribution. "
+                    return self.correlation(col1, col2, 'pearson', significance, report)
+                else:
+                    report += "Samples do not have normal distribution. "
+                    return self.correlation(col1, col2, 'spearman', significance, report)
+        else:
+            return self.correlation(col1, col2, test, significance, report)
+
+    def correlation(self, col1, col2, test, significance, report):
+        statistic, p_value = self.selectors['correlation_test'][test](col1, col2)
+        result = True if p_value < significance else False
+        if result:
+            report += "The alternative hypothesis is accepted, thus there is correlation between the samples. "
+        else:
+            report += "The null hypothesis is accepted, thus there is no correlation between the samples. " 
+        report += "Significance level considered = {},  test applied = {}, p-value = {}, test statistic = {}. ".format(significance, test, p_value, statistic)
+        return TestResult(statistic, p_value, significance, test, result, report)
+
+    def check_binary(self, col):
+        for data in col:
+            if data != 0 and data !=1:
+                return False
+        return True
+
+    def categorical_test(self, col1, col2, significance = 0.05, test = 'default'):
+        """
+        Tests the null hypothesis that the categorical samples (col1,col2) are not dependent
+        
+    	Parameters
+    	----------            
+        col1 : array_like
+                  Array of sample data, must be categorical data.
+        col1 : array_like
+                  Array of sample data, must be categorical data.
+        significance : float
+                               level of significance (default = 0.05)
+        test : string
+                 test to be applied
+                    
+    	Returns
+    	-------
+        TestResult
+        """
+
+        contigency_table = pd.crosstab(col1, col2)
+        normality_condition = True
+        report = ""
+        if test == 'default':
+            for row in contigency_table.iloc:
+                for data in row:
+                    if data <= 5:
+                        normality_condition = False
+                        report += "Normality condition not satisfied (observed and expected frequency in some cell of contigency table <= 5). Chi2 may not be valid. "
+                        break
+                else:
+                    continue
+                break
+            if  normality_condition:
+                report += "Normality condition is satisfied. "
+                return self.categorical(contigency_table, 'chi2', significance, report)
+            else:
+                shape = np.shape(contigency_table)
+                if shape[0] == 2 and shape[1] == 2:
+                    fisher = self.categorical(contigency_table, 'fisher_exact', significance, report)
+                    fisher.chi2 = self.categorical(contigency_table, 'chi2', significance, report)
+                    fisher.report += "To view chi2 results check the chi2 attribute (TestResult.chi2)"
+                    return fisher
+                else:
+                    report += "Contigency table is not 2x2, Fisher exact cannot be used. "
+                    return self.categorical(contigency_table, 'chi2', significance, report)
+        else:
+            return self.categorical(contigency_table, test, significance, report)
+                
+    def categorical(self, contigency_table, test, significance, report):
+        output = self.selectors['compare_2_categorical'][test](contigency_table)
+        statistic, p_value = output[0], output[1]
+        result = True if p_value < significance else False
+        if result:
+            report += "The alternative hypothesis is accepted, thus there is a dependency between the samples. "
+        else:
+            report += "The null hypothesis is accepted, thus the two samples are independent. " 
+        report += "Significance level considered = {},  test applied = {}, p-value = {}, test statistic = {}. ".format(significance, test, p_value, statistic)
+        return TestResult(statistic, p_value, significance, test, result, report)
+        
 
     def normality_test(self, col1, significance = 0.05, test = 'shapiro-wilk'):
         """
@@ -77,7 +212,7 @@ class Tester:
             report += "Warning!!! Test only valid for n>=20 (n = number of samples in data). "
 
         statistic, p_value = self.selectors['normality_test'][test](col1)
-        result = True if p_value > significance else False
+        result = True if p_value >= significance else False
         if result:
             report += "The null hypothesis is accepted, thus the data was  drawn from a normal distribution"
         else:
