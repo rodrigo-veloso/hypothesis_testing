8908a6fedd35cf1ee4ea76766d9c9802cee91361
rodrigo <rodrigoveloso1t@gmail.com>
2021-07-19 11:45:10 -0300

    normality test, app and general updates

--- ml/preprocessing/hypothesis_testing.py
+++ ml/preprocessing/hypothesis_testing.py
@@ -2,61 +2,9 @@ from os import stat
 import pandas as pd
 import numpy as np
 import pingouin as pg
+from pingouin import chi2_independence
 import warnings
-from scipy.stats import shapiro
-from scipy.stats import normaltest
-from scipy.stats import chi2_contingency
 from scipy.stats import fisher_exact
-from scipy.stats import pearsonr
-from scipy.stats import spearmanr
-from scipy.stats import kendalltau
-
-class TestResult:
-
-    def add_result(hypothesis, result, alpha, ci, test, alternative):
-        result["p-value"]=round(test[1],5)
-        result[str((1-alpha)*100)+"% percente confidence interval"]=[round(ci[0],5), round(ci[1],5)]
-        result["H0 - null hypothesis"]=hypothesis[alternative+'_H0']
-        result["H1 - alternative hypothesis"]=hypothesis[alternative+'_H1']
-        # interpret via p-value
-        print(f'p-value = {result["p-value"]}')
-        print(f'alpha = {alpha}')
-        if test[1]> alpha:
-            print(f'Accept null hypothesis')
-
-    def __init__(self, statistic, p_value, alpha, test, result, report):
-        """ 
-        Constructor
-
-        Parameters
-        ----------
-            statistic :          float
-                                     test statistics
-            p-value :           float
-                                     test's p-value
-            alpha :   float
-                                     significance level considered
-            test :                 string
-                                     test applied
-            result :             bool
-                                     test result, True if alternative hypothesis is accepted
-            report :             string
-                                     sumary of the test results
-
-        Returns   
-        ----------
-            TestResult
-        """                 
-
-        self.statistic = statistic
-        self.p_value = p_value
-        self.alpha = alpha
-        self.test = test
-        self.result = result
-        self.report = report
-
-    def __str__(self):
-        return "statistic = {}\n p-value = {}\n sigificance = {}\n test = {}\n result = {}\n report: {}".format(self.statistic,self.p_value,self.alpha,self.test,self.result,self.report)
 
 class Tester:
 
@@ -71,13 +19,8 @@ class Tester:
     	-------
         Tester
         """
-        self.selectors = {'normality_test':{'shapiro-wilk':shapiro,
-                                                                 'dagostinos':normaltest},
-                                    'compare_2_categorical':{'chi2':chi2_contingency,
-                                                                                'fisher_exact':fisher_exact},
-                                    'correlation_test':{'pearson':pearsonr,
-                                                                   'spearman':spearmanr,
-                                                                   'kendalltau':kendalltau}}
+        self.selectors = {'compare_2_categorical':{'chi2':chi2_independence,
+                                                                                'fisher_exact':fisher_exact}}
 
     def correlation_test(self, sample1, sample2, alpha = 0.05, alternative = 'two-sided', method = None, binary = ''):
         """
@@ -98,7 +41,7 @@ class Tester:
                     
     	Returns
     	-------
-        TestResult
+        pd.DataFrame
         """
         sample1, sample2 = np.array(sample1), np.array(sample1) 
         np_types = [np.dtype(i) for i in [np.int32, np.int64, np.float32, np.float64]]
@@ -121,8 +64,8 @@ class Tester:
                 report += "Samples are binary, Pearson correlation is going to be applied (Point-biserial). "
                 return self.correlation(sample1, sample2, 'pearson', alpha, report, alternative)
             else:
-                check1 = self.normality_test(sample1).result
-                check2 = self.normality_test(sample2).result
+                check1 = self.normality_test(sample1).normal[0]
+                check2 = self.normality_test(sample2).normal[0]
                 check = check1 and check2
                 if check:
                     report += "Samples have normal distribution. "
@@ -167,7 +110,7 @@ class Tester:
                     
     	Returns
     	-------
-        TestResult
+        pd.DataFrame
         """
         sample1_array, sample2_array = data[sample1], data[sample2]
         report = ""
@@ -236,7 +179,7 @@ class Tester:
         print()
         
 
-    def normality_test(self, sample, alpha = 0.05, test = 'shapiro-wilk'):
+    def normality_test(self, sample, alpha = 0.05, method = 'shapiro'):
         """
         Tests the null hypothesis that the data was drawn from a normal distribution
         
@@ -251,18 +194,21 @@ class Tester:
                     
     	Returns
     	-------
-        TestResult
+        pd.DataFrame
         """
         report = ""
-        if test == 'dagostinos' and len(sample) < 20:
-            report += "Warning!!! Test only valid for n>=20 (n = number of samples in data). "
-
-        statistic, p_value = self.selectors['normality_test'][test](sample)
-        result = True if p_value >= alpha else False
+        sample = np.array(sample)
+        np_types = [np.dtype(i) for i in [np.int32, np.int64, np.float32, np.float64]]
+        sample_dtypes = sample.dtype
+        if any([not t in np_types for t in [sample_dtypes]]):
+            raise Exception('Non numerical variables... Try using categorical_test method instead.')
+        df = pg.normality(sample)
+        result = True if df['pval'][0] >= alpha else False
         if result:
             report += "The null hypothesis is accepted, thus the data was  drawn from a normal distribution"
         else:
             report += "The alternative hypothesis is accepted, thus the data was not  drawn from a normal distribution. "
-        report += "Significance level considered = {},  test applied = {}, p-value = {}, test statistic = {}".format(alpha, test, p_value, statistic)
-        return TestResult(statistic, p_value, alpha, test, result, report)
+        report += "Significance level considered = {},  test applied = {}, p-value = {}, test statistic = {}".format(alpha, method, df['pval'][0], df['W'][0])
+        df['report'] = report
+        return df
     
